160e58a40062f628b7bb2e9812ec9d3af1aa0349
luauglazer <thedividagamer@gmail.com>
2026-09-10 08:58:15 -0300

    Create YAHRMDELTASCRIPT.lua

---
+++ YAHRMDELTASCRIPT.lua
@@ -0,0 +1,4 @@
+repeat task.wait() until game:IsLoaded()
+if game.PlaceId == 142823291 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/luauglazer/Sort/refs/heads/main/YAHRM.lua', true))("blakejdjdkjfnjfnhfhf")
+end
