f693157bb627e3c7956f9aabac109b0f8ca2df16
luauglazer <thedividagamer@gmail.com>
2026-09-10 09:00:57 -0300

    Update YAHRMDELTASCRIPT.lua

--- YAHRMDELTASCRIPT.lua
+++ YAHRMDELTASCRIPT.lua
@@ -1,4 +1,4 @@
 repeat task.wait() until game:IsLoaded()
 if game.PlaceId == 142823291 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/luauglazer/Sort/refs/heads/main/YAHRM.lua', true))("blakejdjdkjfnjfnhfhf")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/luauglazer/Sort/refs/heads/main/YAHRM.lua', true))("mu9q3aiqv3eo55l07wdw")
 end
